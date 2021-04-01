14a93050fb17634983209e4b94f98f0df41d09a1
Henrique Barbosa <henriqbarbosa@icloud.com>
2021-04-01 11:04:49 -0300

    Add splash screen

---
+++ empresas-ios/empresas-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  empresas-ios
+//
+//  Created by Henrique Barbosa on 01/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
